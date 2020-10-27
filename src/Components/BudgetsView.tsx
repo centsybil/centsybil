b92b9326f49d2db1aabf808b5ce417461d28101e
LordRegis22 <45404541+LordRegis22@users.noreply.github.com>
2020-10-27 16:06:31 -0700

    Import bootstrap components
    Co-authored-by: Joal Kim <joalkims@gmail.com>
    Co-authored-by: Cam Greer <camgreer01@gmail.com>
    Co-authored-by: Binish Ali <binish21@gmail.com>

--- src/Components/BudgetsView.tsx
+++ src/Components/BudgetsView.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { BudgetData, Budget } from '../types';
+import { BudgetDataType, BudgetType } from '../types';
+import Budget from './Budget';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 interface BudgetsViewProps {
-  budgetData: BudgetData;
+  budgetData: BudgetDataType;
 }
 
 function BudgetsView({ budgetData }: BudgetsViewProps) {
-  const budgets = budgetData.budgetsArr.map((budget: Budget) => {
-    return <h1>{budget.budgetName}</h1>;
+  const budgets = budgetData.budgetsArr.map((budget: BudgetType) => {
+    return <Budget singleBudgetData={budget} />;
   });
-  return <div>{budgets}</div>;
+
+  return (
+    <>
+      <div className='container mt-4 center shadow p-4'>
+        <h1>My Budgets</h1>
+        {budgets}
+      </div>
+    </>
+  );
 }
 
 export default BudgetsView;
