61932e7d5e08da76ec4e0eb427415f05783c0642
LordRegis22 <45404541+LordRegis22@users.noreply.github.com>
2020-10-27 10:16:52 -0700

    Update static method to serve to all requests

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import path from 'path';
 const app = express();
 
-app.use('/', express.static('public'));
+app.use(express.static('public'))
 
 app.listen(3000, () =>
   console.log('centSybil ready to dispense sage budget advice on port 3000')
