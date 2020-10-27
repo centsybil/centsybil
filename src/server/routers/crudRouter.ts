f0f6139a36d5afdc6382c0ea15d7244fd5d72e1a
joalk <joalkims@gmail.com>
2020-10-27 15:30:50 -0700

    created routes for crudRouter
    Co-authored-by: Cam Greer <camgreer01@gmail.com>
    Co-authored-by: Binish Ali <binish21@gmail.com>
    Co-authored-by: Michael O'Halloran <45404541+LordRegis22@users.noreply.github.com>

--- src/server/routers/crudRouter.ts
+++ src/server/routers/crudRouter.ts
@@ -1,2 +1,39 @@
+import express from 'express';
+const crudController = require('../controllers/crudController');
+const router = express.Router();
+const path = require('path');
 
+//Login is successful
 
+//Update/edit Total Budget
+router.put('/', crudController.updateTotal, (req, res) => {
+  res.status(200).json({ verified: res.locals.status });
+});
+
+//Category Routes
+router.post('/cat', crudController.addCat, (req, res) => {
+  res.status(200).json({ verified: res.locals.status });
+});
+
+router.put('/cat', crudController.updateCat, (req, res) => {
+  res.status(200).json({ verified: res.locals.status });
+});
+
+router.delete('/cat', crudController.deleteCat, (req, res) => {
+  res.status(200).json({ verified: res.locals.status });
+});
+
+//Transactions Routes
+router.post('/trans', crudController.addTrans, (req, res) => {
+  res.status(200).json({ verified: res.locals.status });
+});
+
+router.put('/trans', crudController.updateTrans, (req, res) => {
+  res.status(200).json({ verified: res.locals.status });
+});
+
+router.delete('/trans', crudController.deleteTrans, (req, res) => {
+  res.status(200).json({ verified: res.locals.status });
+});
+
+module.exports = router;
