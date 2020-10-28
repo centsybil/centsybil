339ac5e9d6afc70f5f430352d26a141de88c8a55
Cameron Greer <camgreer01@gmail.com>
2020-10-28 10:33:11 -0700

    update types.d.ts
    Co-authored-by: Joal Kim <joalkims@gmail.com>
    dquote> Co-authored-by: Binish Ali <binish21@gmail.com>
    Co-authored-by: Michael O'Halloran <45404541+LordRegis22@users.noreply.github.com>

--- src/types.d.ts
+++ src/types.d.ts
@@ -30,3 +30,22 @@ export interface errObj {
   status: number;
   message: { err: string };
 }
+
+export interface BudgetItemType {
+  name: string;
+  price: number;
+  date: string;
+}
+
+export interface BudgetType {
+  budgetName: string;
+  budgetMax: number;
+  budgetWarning: number;
+  currTotal: number;
+  budgetItems: BudgetItemType[];
+}
+
+export interface BudgetDataType {
+  username: string;
+  budgetsArr: BudgetType[];
+}
