a3e2ca2aa4b05829b28304f4f9432c121770e53f
LordRegis22 <45404541+LordRegis22@users.noreply.github.com>
2020-10-29 09:55:55 -0700

    Create conditional rendering for authentication
    Co-authored-by: Joal Kim <joalkims@gmail.com>
    Co-authored-by: Cam Greer <camgreer01@gmail.com>
    Co-authored-by: Binish Ali <binish21@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React, { useContext } from 'react';
+import Authentication from './Components/Authentication';
 import BudgetsView from './Components/BudgetsView';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { Context } from './ContextProvider';
+import dummyData from './dummyData';
+
 function App() {
   const { userData } = useContext(Context);
   return (
@@ -11,10 +14,17 @@ function App() {
         <Navbar.Brand href='#home'>centSybil</Navbar.Brand>
         <Nav className='ml-auto'>
           <Nav.Link href='#about'>About</Nav.Link>
-          <Nav.Link href='#logout'>Logout</Nav.Link>
+          {userData.username && <Nav.Link href='#logout'>Logout</Nav.Link>}
         </Nav>
       </Navbar>
-      <BudgetsView budgetData={userData} />;
+      <div className='container mx-auto my-5 d-flex flex-column align-items-center justify-content-center border rounded shadow'>
+        <img src='/centSybilLogo.png'></img>
+        {userData.username ? (
+          <BudgetsView budgetData={userData} />
+        ) : (
+          <Authentication />
+        )}
+      </div>
     </>
   );
 }
