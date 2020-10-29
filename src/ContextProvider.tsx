217122464fc8564e3e1e82e3e466080a81d21cab
Binish Ali <binish21@gmail.com>
2020-10-28 20:06:31 -0700

    Create Context Provider
    Co-authored-by: Joal Kim <joalkims@gmail.com>
    Co-authored-by: Cam Greer <camgreer01@gmail.com>
    Co-authored-by: Michael O'Halloran <45404541+LordRegis22@users.noreply.github.com>

---
+++ src/ContextProvider.tsx
@@ -0,0 +1,30 @@
+import React, { useState, createContext } from 'react';
+import dummyData from './dummyData';
+
+const Context = createContext();
+
+function ContextProvider(props) {
+  const [userData, setUserData] = useState(dummyData);
+  const createBudget = (name, max, budgetWarning) => {
+    console.log('im here: ', name, max, budgetWarning);
+    const budgetObj = {
+      budgetName: name,
+      budgetMax: max,
+      currTotal: 0,
+      budgetWarning,
+      budgetItems: [],
+    };
+    const userDataCopy = Object.assign(userData);
+    userDataCopy.budgetsArr.push(budgetObj);
+    console.log(userDataCopy, 'here');
+    setUserData(userDataCopy);
+  };
+
+  return (
+    <Context.Provider value={{ userData, setUserData, createBudget }}>
+      {props.children}
+    </Context.Provider>
+  );
+}
+
+export { Context, ContextProvider };
